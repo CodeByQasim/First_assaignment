5c7033daeb136c3b2f2812060fa05fab054162a9
CodeByQasim <qaximkhaskheli@gmail.com>
2024-10-16 22:39:15 +0500

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	//declared variable for integer value
+	int a, b;
+    //here input value of both variables from user 
+	printf("Enter the first integer Number : ");
+	scanf("%d",&a);
+	printf("Enter the second integer Number : ");
+	scanf("%d",&b);
+	//display result of sum 
+	printf("the sum of 1st and 2nd integer is : %d",a+b);
+	getch();
+	
+}
