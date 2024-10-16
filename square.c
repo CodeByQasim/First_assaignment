e39eb5197f2ee7481262c865aa7f9d6abfa1b97e
CodeByQasim <qaximkhaskheli@gmail.com>
2024-10-16 22:35:38 +0500

    Add files via upload

---
+++ square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	//declared varible
+	int num ;
+	//here input value of variables from user
+	printf("Put your integer number : ");
+	scanf("%d",&num);
+	// Display the result in square
+	printf("The square of your integer number is : %d",num*num);
+	getch();
+}
